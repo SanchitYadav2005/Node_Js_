f1f0d75b6d308eabfe379dfb9a382cf0c6b6698f
SanchitYadav2005 <sanchit2005yudavanshi@gmail.com>
2022-08-17 15:24:34 +0530

    done

--- NPM/Jokester/index.js
+++ NPM/Jokester/index.js
@@ -1,17 +1,18 @@
 // requiring the package.
 const giveMeAJoke = require("give-me-a-joke");
+const getColor = require("colors")
 // funciton to get random dad joke.
 giveMeAJoke.getRandomDadJoke(function(randomDadJoke){
-    console.log(randomDadJoke)
+    console.log(randomDadJoke.rainbow)
 })
 // fuction to get reandom joke of the day.
 giveMeAJoke.getRandomJokeOfTheDay(function(randomJoke){
-    console.log(randomJoke)
+    console.log(randomJoke.green)
 })
 // function to get costumized joke. This requires first name and last name.
 const firstName = "sanchit";
 const lastName = "yadav";
 
 giveMeAJoke.getCustomJoke(firstName, lastName, function(costumizedJoke){
-    console.log(costumizedJoke)
+    console.log(costumizedJoke.blue)
 })
